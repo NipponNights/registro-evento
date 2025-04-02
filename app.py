fd5b95a107a3774dc67752e93a5f8d21136d9915
NipponNights <bodouk.business@gmail.com>
2025-04-02 21:10:23 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -33,6 +33,19 @@ def register():
     except sqlite3.IntegrityError:
         message = "✖ Este correo ya está registrado."
     return render_template("index.html", message=message)
+from flask import request, abort
+
+@app.route("/usuarios")
+def ver_usuarios():
+    password = request.args.get("pass")
+    if password != "1ll0capor_06":
+        abort(403)
+    with sqlite3.connect("nippon.db") as conn:
+        c = conn.cursor()
+        c.execute("SELECT email, fecha_registro FROM usuarios")
+        usuarios = c.fetchall()
+    return "<h2>Usuarios registrados:</h2><br>" + "<br>".join([f"{email} - {fecha}" for email, fecha in usuarios])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
